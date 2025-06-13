61d890b37644b1ef5237db791af737e34f7732a1
Saloni Nandurkar <saloninandurkar@gmail.com>
2025-06-13 13:07:22 +0530

    Create print_n_times.java

---
+++ 6-Print Name N times using Recursion/print_n_times.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    void printGfg(int N) {
+        
+        if( N<=0)
+        return;
+        
+        System.out.print("GFG ");
+        
+        printGfg(N-1);
+        
+    }
+}
